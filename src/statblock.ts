2b378cd439efe146dc9e9d07d719bb988f9b0ab5
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-05 04:53:10 +0300

    Delete src/statblock.ts

--- src/statblock.ts
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import "./_css/main.scss";
-import { StatblockPopover } from "./components/statblock/StatblockPopover.tsx";
-
-const root = ReactDOM.createRoot(<HTMLElement>document.querySelector("#app"));
-root.render(React.createElement(StatblockPopover));
