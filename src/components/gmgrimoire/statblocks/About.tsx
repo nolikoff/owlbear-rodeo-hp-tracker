e77eb87d9964d190879dfb8d1974432d731a18ce
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-05 04:35:51 +0300

    Update About.tsx

--- src/components/gmgrimoire/statblocks/About.tsx
+++ src/components/gmgrimoire/statblocks/About.tsx
@@ -1,6 +1,5 @@
 import { useLocalStorage } from "../../../helper/hooks.ts";
 import { ID } from "../../../helper/variables.ts";
-import { DiceButtonWrapper, Stats } from "../../general/DiceRoller/DiceButtonWrapper.tsx";
 import { E5Statblock } from "../../../api/e5/useE5Api.ts";
 import { PfStatblock } from "../../../api/pf/usePfApi.ts";
 import { useMetadataContext } from "../../../context/MetadataContext.ts";
@@ -29,24 +28,7 @@ export const About = ({
             {!hideTitle ? <h3>About</h3> : null}
             <button className={`expand ${open ? "open" : null}`} onClick={() => setOpen(!open)}></button>
             <div className={`about-content-wrapper ${open ? "open" : "hidden"}`}>
-                <div className={"about-content"}>
-                    <DiceButtonWrapper
-                        text={about}
-                        context={context || statblock?.name || "Custom Roll"}
-                        statblock={statblock?.name}
-                        stats={
-                            stats || {
-                                strength: statblock?.stats.strength || 0,
-                                dexterity: statblock?.stats.dexterity || 0,
-                                constitution: statblock?.stats.constitution || 0,
-                                intelligence: statblock?.stats.intelligence || 0,
-                                wisdom: statblock?.stats.wisdom || 0,
-                                charisma: statblock?.stats.charisma || 0,
-                            }
-                        }
-                        proficiencyBonus={room?.ruleset === "e5" ? (statblock as E5Statblock).proficiency_bonus : null}
-                    />
-                </div>
+                <div className={"about-content"}></div>
             </div>
         </div>
     ) : null;
