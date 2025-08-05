55a4c410da24bcd937a6b5baf1c64433721f0e21
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-05 19:52:51 +0300

    Update contextMenuItems.ts

--- src/background/contextMenuItems.ts
+++ src/background/contextMenuItems.ts
@@ -38,7 +38,7 @@ function createPlayerMenu(
             {
               key: [
                 "metadata",
-                "com.owlbear-rodeo-bubbles-extension/metadata",
+                "com.owlbear-rodeo-hp-tracker/metadata",
                 "hide",
               ],
               value: true,
