2c524b3b6faba3562287ba30d63c715ab35c5d1f
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-05 04:38:37 +0300

    Update Statblock.tsx

--- src/components/gmgrimoire/statblocks/Statblock.tsx
+++ src/components/gmgrimoire/statblocks/Statblock.tsx
@@ -1,8 +1,6 @@
 import { GMGMetadata } from "../../../helper/types.ts";
 import { useMetadataContext } from "../../../context/MetadataContext.ts";
 import { useTokenListContext } from "../../../context/TokenContext.tsx";
-import { E5StatBlockWrapper } from "./e5/E5Statblock.tsx";
-import { PFStatBlockWrapper } from "./pf/PfStatblock.tsx";
 import { useShallow } from "zustand/react/shallow";
 
 export const Statblock = ({ id }: { id: string }) => {
@@ -11,12 +9,6 @@ export const Statblock = ({ id }: { id: string }) => {
     const data = token?.data as GMGMetadata;
 
     return token ? (
-        <div className={"statblock-wrapper"}>
-            {room && room.ruleset === "e5" ? (
-                <E5StatBlockWrapper slug={data.sheet} itemId={id} />
-            ) : (
-                <PFStatBlockWrapper slug={data.sheet} itemId={id} />
-            )}
-        </div>
+        <div className={"statblock-wrapper"}></div>
     ) : null;
 };
