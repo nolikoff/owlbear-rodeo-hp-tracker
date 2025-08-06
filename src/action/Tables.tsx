b99cc72b501da8cca67b38784bbe530696363acb
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-07 01:48:36 +0300

    Update Tables.tsx

--- src/action/Tables.tsx
+++ src/action/Tables.tsx
@@ -115,44 +115,6 @@ export function SceneTokensTable({
         strategy={verticalListSortingStrategy}
       >
         <Table tabIndex={-1}>
-          <TableHeader>
-            <TableRow>
-              {appState.operation !== "none" && (
-                <CheckboxTableHead
-                  included={allChecked(tokens, appState.includedItems)}
-                  onCheckedChange={(checked) =>
-                    dispatch({
-                      type: "set-included-items",
-                      includedItems: new Map(
-                        tokens.map((token) => [token.item.id, checked]),
-                      ),
-                    })
-                  }
-                />
-              )}
-              <TableHead>Token</TableHead>
-              {appState.operation === "none" && playerRole === "GM" && (
-                <TableHead>Access</TableHead>
-              )}
-              {appState.operation === "none" && playerRole === "GM" && (
-                <TableHead>Owner</TableHead>
-              )}
-              {appState.operation !== "damage" && (
-                <TableHead title="Hit Points / Maximum Hit Points, Temporary Hit Points">
-                  Stats
-                </TableHead>
-              )}
-              {appState.operation === "damage" && (
-                <>
-                  <TableHead>Multiplier</TableHead>
-                  <TableHead>Damage</TableHead>
-                  <TableHead>New Hit Points</TableHead>
-                </>
-              )}
-            </TableRow>
-          </TableHeader>
-        </Table>
-        <Table tabIndex={-1}>
           <TableBody>
             {tokens.map((token) => {
               const included = getIncluded(
