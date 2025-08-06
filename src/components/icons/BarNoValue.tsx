c010ac31e6f19909b7b3eeb016a1e3276a5bc3b2
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-06 19:42:03 +0300

    Update BarNoValue.tsx

--- src/components/icons/BarNoValue.tsx
+++ src/components/icons/BarNoValue.tsx
@@ -1,6 +1,7 @@
 export const BarNoValue = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
     width="24px"
     height="24px"
     viewBox="0 0 40 24"
