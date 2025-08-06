14b6c0456c9041ad58081640d23117a73ec83ee3
Roman Novikov <58274176+nolikoff@users.noreply.github.com>
2025-08-06 07:24:59 +0300

    Update Tables.tsx

--- src/action/Tables.tsx
+++ src/action/Tables.tsx
@@ -263,9 +263,9 @@ export function SceneTokensTable({
                             className={
                               "select-owner flex rounded-md border px-2 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-mirage-500 focus-visible:border-transparent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50 dark:border-mirage-800 dark:placeholder:text-mirage-400 dark:focus-visible:ring-primary-dark h-[32px] w-full"
                             }
-                            style={
-                              "background: transparent;"
-                            }
+                            style={{
+                              background: "transparent",
+                            }}
                         >
                             <option value={OBR.player.id}>GM</option>
                             {players.map((player) => {
